b84eaba29bc91bbf2d3c30efe5dd47c5ac8a6eda
moaaz malek <madhalfrwat@gmail.com>
2026-04-30 00:15:01 +0300

    Initial the project with EF-Core

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        :DbContext(options)
+    {
+
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = default!;
+        public int DisplayOrder { get; set; }
+
+    }
+}
