1dae291c1b12e1c5686abfe638eeac6167e01e66
moaaz malek <madhalfrwat@gmail.com>
2026-05-01 04:07:52 +0300

    Add Full CRUD operations for Catgory

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿
+using BulkyWeb.Models;
 using Microsoft.EntityFrameworkCore;
 namespace BulkyWeb.Data
 {
@@ -6,6 +7,14 @@ namespace BulkyWeb.Data
         DbContextOptions<ApplicationDbContext> options)
         :DbContext(options)
     {
-
+        public  DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId =1,Name="Action",DisplayOrder=1},
+                new Category { CategoryId =2,Name="SciFi",DisplayOrder=2},
+                new Category { CategoryId =3,Name="History",DisplayOrder=3}
+                );
+        }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,12 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 namespace BulkyWeb.Models
 {
     public class Category
     {
         
         public int CategoryId { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; } = default!;
+        [Range(1,100,ErrorMessage ="Display Order must be between 1-100 ")]
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
 
     }
